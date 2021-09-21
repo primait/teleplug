9b8d93ddcf7cbbe21bba08a8c6213858ecf014d4
Matteo Busi <matteo.busi@prima.it>
2021-09-21 17:26:12 +0200

    Update opentelemetry deps (#5)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Teleplug.MixProject do
   def project do
     [
       app: :teleplug,
-      version: "0.1.0",
+      version: "1.0.0-rc.1",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -22,10 +22,8 @@ defmodule Teleplug.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:opentelemetry_api, "~> 0.6.0"},
-      {:opentelemetry, "~> 0.6.0", only: :test},
+      {:opentelemetry_api, "~> 1.0.0-rc.2"},
+      {:opentelemetry, "~> 1.0.0-rc.2", only: :test},
       {:plug, "~> 1.11"},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.1", only: [:dev, :test], runtime: false},
