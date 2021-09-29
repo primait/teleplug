6955994a2036896e6603ae258fadd254c00e127e
Matteo Busi <mbusi81@gmail.com>
2021-09-29 15:03:26 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Teleplug.MixProject do
   def project do
     [
       app: :teleplug,
-      version: "1.0.0-rc.1",
+      version: "1.0.0-rc.2",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
