968720cac23eaf53e39996aaf800ec33509ec26d
Matteo Busi <mbusi81@gmail.com>
2021-10-25 12:21:46 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Teleplug.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/primait/teleplug"
-  @version "1.0.0-rc.7"
+  @version "1.0.0-rc.8"
 
   def project do
     [
